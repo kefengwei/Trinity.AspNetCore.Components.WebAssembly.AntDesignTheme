1247547b83b2c87868851a8fa485320295d7b316
kefengwei <kefengwei@live.com>
2021-01-08 16:29:53 +0800

    Init

---
+++ src/Volo.Abp.AspNetCore.Components.WebAssembly.AntDesignTheme/AbpAspNetCoreComponentsWebAssemblyBasicTheme.cs
@@ -0,0 +1,30 @@
+﻿using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+using Volo.Abp.AspNetCore.Components.WebAssembly.Theming.Routing;
+using Volo.Abp.AspNetCore.Components.WebAssembly.Theming.Toolbars;
+using Volo.Abp.Modularity;
+using AntDesign;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly.AntDesignTheme
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule)
+        )]
+    public class AbpAspNetCoreComponentsWebAssemblyAntDesignThemeModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAntDesign();
+            
+            Configure<AbpRouterOptions>(options =>
+            {
+                options.AdditionalAssemblies.Add(typeof(AbpAspNetCoreComponentsWebAssemblyAntDesignThemeModule).Assembly);
+            });
+
+            Configure<AbpToolbarOptions>(options =>
+            {
+                options.Contributors.Add(new AntDesignThemeToolbarContributor());
+            });
+        }
+    }
+}
---
+++ src/Volo.Abp.AspNetCore.Components.WebAssembly.AntDesignTheme/BasicThemeBundleContributor.cs
@@ -0,0 +1,17 @@
+﻿using Volo.Abp.Bundling;
+
+namespace Volo.Abp.AspNetCore.Components.WebAssembly.AntDesignTheme
+{
+    public class AntDesignThemeBundleContributor : IBundleContributor
+    {
+        public void AddScripts(BundleContext context)
+        {
+            context.Add("_content/AntDesign/js/ant-design-blazor.js");
+        }
+
+        public void AddStyles(BundleContext context)
+        {
+            context.Add("_content/AntDesign/css/ant-design-blazor.css");
+        }
+    }
+}
